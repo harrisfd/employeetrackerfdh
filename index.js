20c829bd6c4d8c194a5ea9cdaa19c6032d99e5ea
ldylicia <fharrisfoster@gmail.com>
2021-05-01 20:11:40 -0700

    added different role tables

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const inquirer = require("inquirer")
-const connection=require("./db/connection")
+const connection = require("./db/connection")
 
-connection.connect(error =>{
-    if(error)throw error 
+connection.connect(error => {
+    if (error) throw error
     console.log("connected to mysql")
     init()
 
@@ -15,7 +15,7 @@ function init() {
         message: "Please Select From the Following Options:",
         choices: [
             "view all departments", "view all roles", "view all employees", "add a department", "add a role",
-            "add an employee", "update an employee role","exit",
+            "add an employee", "update an employee role", "exit",
         ]
     }).then(answer => {
         console.log(answer.selection)
@@ -41,34 +41,170 @@ function init() {
             case "update an employee role":
                 updateEmployeerole()
                 break;
-                case "exit":
+            case "exit":
                 connection.end()
                 break;
             default:
                 break;
         }
     })
+    //add view roles and view employees
 }
 
 function viewDepartments() {
-    connection.query("SELECT * FROM department", (err, res)=>{
-        if(err)throw err 
+    connection.query("SELECT * FROM department", (err, res) => {
+        if (err) throw err
         console.table(res)
+        init()
     })
 }
-//add view roles and view employees
+function viewRoles() {
+    connection.query("SELECT * FROM role", (err, res) => {
+        if (err) throw err
+        console.table(res)
+        init()
+    })
+}
+function viewEmployees() {
+    connection.query("SELECT * FROM employee", (err, res) => {
+        if (err) throw err
+        console.table(res)
+        init()
+    })
+}
+
 function addDepartment() {
     inquirer.prompt({
-        type:"input",
-        name:"departmentname",
-        message:"What Department Name do you want to add?"
-    }).then(newname=>{
+        type: "input",
+        name: "departmentname",
+        message: "What Department Name do you want to add?"
+    }).then(newname => {
         connection.query("INSERT INTO department SET ?", {
-            name:newname.departmentname
-        }, (err,res)=>{
-            if(err)throw err 
+            name: newname.departmentname
+        }, (err, res) => {
+            if (err) throw err
             console.log("department name added successfully")
         })
     })
-    
+
+}
+
+function addRole() {
+    connection.query("SELECT * FROM department", (err, res) => {
+        inquirer.prompt([{
+            type: "input",
+            name: "rolename",
+            message: "What Role Name do you want to add?"
+        },
+        {
+            type: "input",
+            name: "salaryinput",
+            message: "What is the Salary of your Role?"
+        },
+        {
+            type: "list",
+            name: "departmentselection",
+            message: "What is the Department this role belongs to?",
+            choices: res
+        }
+        ]).then(newname => {
+           
+            
+            const depobj=res.find(department=>department.name===newname.departmentselection)
+            console.log(depobj)
+            connection.query("INSERT INTO role SET ?", {
+                title: newname.rolename,
+                salary:newname.salaryinput,
+                department_id:depobj.id 
+            }, (err, res) => {
+                if (err) throw err
+                console.log("Role added successfully")
+            })
+        })
+    })
+}
+
+function addEmployee() {
+    connection.query("SELECT id, CONCAT(first_name,' ', last_name) AS name FROM employee", (err, emplres) => {
+        connection.query("SELECT id,title AS name FROM role", (err, roleres) => {
+        inquirer.prompt([{
+            type: "input",
+            name: "firstname",
+            message: "What is the first name?"
+        },
+        {
+            type: "input",
+            name: "lastname",
+            message: "What is the last name?"
+        },
+        {
+            type: "list",
+            name: "roleselection",
+            message: "What is the Role name?",
+            choices: roleres
+        },
+        {
+            type:"list",
+            name:"deptmanager",
+            message:"What is the manager's name?",
+            choices:emplres
+        }
+        ]).then(newname => {
+           
+            
+            const roleobj=roleres.find(role=>role.name===newname.roleselection)
+            const empobj=emplres.find(employee=>employee.name===newname.deptmanager)
+            
+         
+            connection.query("INSERT INTO employee SET ?", {
+                first_name: newname.firstname,
+                last_name:newname.lastname,
+               role_id:roleobj.id, 
+                manager_id:empobj.id 
+            }, (err, res) => {
+                if (err) throw err
+                console.log("Employee added successfully")
+                init()
+            })
+        })
+    })
+})
+}
+//which employee do you want to update and what is the employee's new role?
+function addEmployee() {
+    connection.query("SELECT id, CONCAT(first_name,' ', last_name) AS name FROM employee", (err, emplres) => {
+        connection.query("SELECT id,title AS name FROM role", (err, roleres) => {
+        inquirer.prompt([
+        {
+            type: "list",
+            name: "roleselection",
+            message: "What is the Role name?",
+            choices: roleres
+        },
+        {
+            type:"list",
+            name:"deptmanager",
+            message:"What is the manager's name?",
+            choices:emplres
+        }
+        ]).then(newname => {
+           
+            
+            const roleobj=roleres.find(role=>role.name===newname.roleselection)
+            const empobj=emplres.find(employee=>employee.name===newname.deptmanager)
+            
+         
+            connection.query("INSERT INTO employee SET ?", {
+                first_name: newname.firstname,
+                last_name:newname.lastname,
+               role_id:roleobj.id, 
+                manager_id:empobj.id 
+            }, (err, res) => {
+                if (err) throw err
+                console.log("Employee added successfully")
+                init()
+            })
+        })
+    })
+})
 }
